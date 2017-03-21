d56d41dec51e5d0f7943101c70773562e930a46a
Edc.zhang <zhangjian_0419@sina.com>
2017-03-21 13:58:06 +0800

    Update FileOpener2.java

--- src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
+++ src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
@@ -104,7 +104,7 @@ public class FileOpener2 extends CordovaPlugin {
 				if(Build.VERSION.SDK_INT>=24) { //判读版本是否在7.0以上
 					//参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
 					Uri apkUri =
-							FileProvider.getUriForFile(cordova.getActivity(), "com.tdenergys.evopYangJing.fileprovider", file);
+							FileProvider.getUriForFile(cordova.getActivity(), "com.xxxx.XXXXX.fileprovider", file);
 					//添加这一句表示对目标应用临时授权该Uri所代表的文件
 					intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 					intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
